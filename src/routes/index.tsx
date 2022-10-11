a7889e54ac23e7d9f04530d45d65cfa7d49e0393
Petro Silenius <petro.silenius@gmail.com>
2022-10-11 23:19:36 +0300

    Create view for 1st step of Slack OAuth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,135 +1,28 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, Resource, useResource$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const usersResource = useResource$(() => getUsers());
+
   return (
     <div>
-      <h1>
-        Welcome to Qwik <span class="lightning">⚡️</span>
-      </h1>
-
-      <ul>
-        <li>
-          Check out the <code>src/routes</code> directory to get started.
-        </li>
-        <li>
-          Add integrations with <code>npm run qwik add</code>.
-        </li>
-        <li>
-          More info about development in <code>README.md</code>
-        </li>
-      </ul>
-
-      <h2>Commands</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href="https://developers.cloudflare.com/pages" target="_blank">
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href="https://expressjs.com/" target="_blank">
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href="https://docs.netlify.com/" target="_blank">
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static-node</code>
-          </td>
-          <td>
-            <a
-              href="https://qwik.builder.io/qwikcity/static-site-generation/overview/"
-              target="_blank"
-            >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-      </table>
-
-      <h2>Community</h2>
-
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href="https://qwik.builder.io/chat" target="_blank">
-            Chat on discord!
-          </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href="https://twitter.com/QwikDev" target="_blank">
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href="https://github.com/BuilderIO/qwik" target="_blank">
-            Github
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href="https://qwik.builder.io/media/" target="_blank">
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
-      <Link class="mindblow" href="/flower">
-        Blow my mind 🤯
-      </Link>
+      <a href="https://slack.com/oauth/v2/authorize?user_scope=users:read&client_id=3435648499334.4209141463556">
+        Go to Slack
+      </a>
+      <Resource
+        value={usersResource}
+        onPending={() => <>Loading...</>}
+        onRejected={(error) => <>Error: {error.message}</>}
+        onResolved={(users) => (
+          <ul>
+            {users.map((user) => (
+              <li>
+                {user.name} - {user.email}
+              </li>
+            ))}
+          </ul>
+        )}
+      />
     </div>
   );
 });
@@ -137,3 +30,13 @@ export default component$(() => {
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
 };
+
+export async function getUsers(): Promise<string[]> {
+  console.log('FETCH');
+  const resp = await fetch('https://slack.com/api/users.list');
+  console.log('FETCH resolved');
+  const json = await resp.json();
+  const users = json.members;
+
+  return Array.isArray(users) ? users : Promise.reject(json);
+}
