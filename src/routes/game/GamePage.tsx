303ac014d06784ee492cd694d759ce14d4ae9c2d
Petro Silenius <petro.silenius@gmail.com>
2022-10-14 00:27:55 +0300

    Fix type and linting issues, use event.key instead of event.keyCode

--- src/routes/game/GamePage.tsx
+++ src/routes/game/GamePage.tsx
@@ -7,15 +7,21 @@ interface IState {
   score: number;
 }
 
-export const shuffle = (array: User[]) =>  {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+export const shuffle = (array: User[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 
-    return array;
+  return array;
+};
+
+export enum KeyToIndex {
+  'ArrowUp' = 0,
+  'ArrowLeft' = 1,
+  'ArrowRight' = 2,
+  'ArrowDown' = 3,
 }
-
 
 export default component$(({ members }: { members: User[] }) => {
   const state = useStore<IState>({
@@ -26,34 +32,37 @@ export default component$(({ members }: { members: User[] }) => {
   const randomMembers = shuffle(members).slice(0, 4);
   const correctMember = randomMembers[Math.floor(state.generatorNumber * 4)];
 
-  const onClick = $((e) => {
-    const clickedButton = (e.target as HTMLButtonElement);
-    if (clickedButton.value === randomMembers.indexOf(correctMember)) {
-      state.score ++;
+  const onClick = $((event: MouseEvent) => {
+    const clickedButton = event.target as HTMLButtonElement;
+    if (Number(clickedButton.value) === randomMembers.indexOf(correctMember)) {
+      state.score++;
     }
     state.generatorNumber = Math.random();
-  })
+  });
 
-  const onKeyDown = $((e) => {
-    const index = e.keyCode - 37;
+  const onKeyDown = $((event: KeyboardEvent) => {
+    const pressedKey = event.key as keyof typeof KeyToIndex;
+    const index = KeyToIndex[pressedKey];
 
-    if (index > 3 || index < 0) {
-      return
+    if (!index || typeof index !== 'number') {
+      return;
     }
 
     if (index === randomMembers.indexOf(correctMember)) {
-      state.score ++;
+      state.score++;
     }
     state.generatorNumber = Math.random();
-  })
+  });
 
   return (
     <div class="container">
       <img src={correctMember.profile.image_192} />
       <div className="options" document:onKeyDown$={onKeyDown}>
-      {randomMembers.map((member, index) => (
-        <button onClick$={onClick} value={index}>{member.real_name}</button>
-      ))}
+        {randomMembers.map((member, index) => (
+          <button onClick$={onClick} value={index}>
+            {member.real_name}
+          </button>
+        ))}
       </div>
       <p>{state.score}</p>
     </div>
